a8cd88b30a85c66b4d2b6054a55a0efc9a1fa422
Mahshid Alamdari <168198746+Mahshid98Alm@users.noreply.github.com>
2025-04-28 05:39:16 +0330

    Update urls.py

--- buildings/urls.py
+++ buildings/urls.py
@@ -1,18 +1,7 @@
 from django.urls import path
-from .views import (APIRoot,
-    BuildingListCreateView,
-    BuildingDetailView,
-    CityListCreateView,
-    CityDetailView,)
+from .views import BragaBuildingListView, LisbonBuildingListView
 
 urlpatterns = [
-    path('', APIRoot.as_view(), name='api-root'),
-    
-    # Building endpoints
-    path('buildings/', BuildingListCreateView.as_view(), name='building-list'),
-    path('buildings/<uuid:pk>/', BuildingDetailView.as_view(), name='building-detail'),
-    
-    # City endpoints
-    path('cities/', CityListCreateView.as_view(), name='city-list'),
-    path('cities/<uuid:pk>/', CityDetailView.as_view(), name='city-detail'),
+    path('braga-buildings/', BragaBuildingListView.as_view(), name='braga_buildings'),
+    path('lisbon-buildings/', LisbonBuildingListView.as_view(), name='lisbon_buildings'),
 ]
