225493e28430caa6bcc76b6ce677af235fff3971
Mahshid Alamdari <168198746+Mahshid98Alm@users.noreply.github.com>
2025-04-28 05:38:25 +0330

    models V1.py

--- buildings/models.py
+++ buildings/models.py
@@ -1,23 +1,30 @@
-import uuid
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
-class City(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    population = models.PositiveIntegerField(null=True, blank=True)  # Optional field
-
-    def __str__(self):
-        return self.name
-
-class Building(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=200, blank=True)
-    address = models.CharField(max_length=300, blank=True)
-    # Relate a building to a city. Allow null if you don't always have a city.
-    city = models.ForeignKey(City, related_name='buildings', on_delete=models.CASCADE, null=True, blank=True)
-    geometry = models.JSONField(blank=True, null=True)
-    osm_data = models.JSONField(blank=True, null=True)
+# Model for the Braga buildings table having the four specified columns.
+class BragaBuildingFinal(models.Model):
+    cluster_id = models.IntegerField(unique=True)
+    height = models.FloatField()
+    estimate_height = models.FloatField()
+    geometry = models.GeometryField(srid=4326)  # Use the appropriate SRID
 
     def __str__(self):
-        return self.name or str(self.id)
+        return f"Braga Cluster {self.cluster_id}"
+
+    class Meta:
+        managed = False  # Django will not manage (create/modify) this table.
+        db_table = 'braga_buidings_final'
+
+
+# Model for the Lisbon buildings table having the four specified columns.
+class LisbonBuildingFinal(models.Model):
+    cluster_id = models.IntegerField(unique=True)
+    height = models.FloatField()
+    estimate_height = models.FloatField()
+    geometry = models.GeometryField(srid=4326)
+
+    def __str__(self):
+        return f"Lisbon Cluster {self.cluster_id}"
+
+    class Meta:
+        managed = False
+        db_table = 'lisbon_buidings_final'
